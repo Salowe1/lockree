f5657e51866a912d79ab4e0a8913bd39887a57cf
Salowe <adouaboucedric9@gmail.com>
2024-08-11 16:57:43 +0000

    Removed lockre submodule

---
+++ ios/Runner/GeneratedPluginRegistrant.h
@@ -0,0 +1,22 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+// Import the Firestore plugin header
+#import <cloud_firestore/FLTFirebaseFirestorePlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  if ([registry hasPlugin:@"FLTFirebaseFirestorePlugin"]) {
+    [FLTFirebaseFirestorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseFirestorePlugin"]];
+  }
+  
+  // Register other plugins here if needed
+}
+
+@end
