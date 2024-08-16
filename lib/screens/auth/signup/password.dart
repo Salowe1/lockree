9c7c36c8155025dc43cc1d1015475bcea2ab222d
Salowe Cedric Martinien Adouabou <95149239+Salowe1@users.noreply.github.com>
2024-08-16 19:10:46 +0000

    Update password.dart

--- lib/screens/auth/signup/password.dart
+++ lib/screens/auth/signup/password.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:lockre/constants/colors.dart';
 import 'package:lockre/screens/home/home.dart';
 
