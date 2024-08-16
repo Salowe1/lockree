48de54f5cdd367714fa7b1494ad27f9c93ae01c2
Salowe Cedric Martinien Adouabou <95149239+Salowe1@users.noreply.github.com>
2024-08-16 19:54:26 +0000

    Update reset_password.dart

--- lib/screens/auth/reset/reset_password.dart
+++ lib/screens/auth/reset/reset_password.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
 
 class ChangePasswordScreen extends StatefulWidget {
-  const ChangePasswordScreen({Key? key}) : super(key: key});
+  const ChangePasswordScreen({Key? key}) : super(key: key);
 
   @override
   _ChangePasswordScreenState createState() => _ChangePasswordScreenState();
